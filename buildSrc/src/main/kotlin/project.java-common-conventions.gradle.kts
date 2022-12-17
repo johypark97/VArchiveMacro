596cc0aeec2ef73c0f1c4e9766cfd601586ad14b
johypark97 <johypark97@gmail.com>
2022-12-17 19:23:15 +0900

    Update Gradle version from 7.5.1 to 7.6

--- buildSrc/src/main/kotlin/project.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/project.java-common-conventions.gradle.kts
@@ -8,10 +8,10 @@ repositories {
 
 dependencies {
     // This dependency is used by the application.
-    // implementation("com.google.guava:guava:31.0.1-jre")
+    // implementation("com.google.guava:guava:31.1-jre")
 
     // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 }
 
 java {
