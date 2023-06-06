1d3f72e9d9f9a7aec2448850c4dea57708623fed
johypark97 <johypark97@gmail.com>
2023-06-06 16:23:26 +0900

    Update the version to alpha3

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,6 +1,6 @@
 object Version {
     private const val ROOT_PROJECT = "1.1.0"
-    private const val ROOT_PROJECT_SUFFIX = "alpha2"
+    private const val ROOT_PROJECT_SUFFIX = "alpha3"
 
     fun makeVersionString(version: String = ""): String {
         val list = mutableListOf(ROOT_PROJECT)
