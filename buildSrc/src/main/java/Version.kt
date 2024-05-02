26f31a8113e4a36c57dfeff5b3bd6b086c2f6c22
johypark97 <johypark97@gmail.com>
2024-05-02 17:43:03 +0900

    Update the version to v1.1.0

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,6 +1,6 @@
 object Version {
     private const val ROOT_PROJECT = "1.1.0"
-    private const val ROOT_PROJECT_SUFFIX = "beta2.1"
+    private const val ROOT_PROJECT_SUFFIX = ""
 
     fun makeVersionString(version: String = ""): String {
         val list = mutableListOf(ROOT_PROJECT)
