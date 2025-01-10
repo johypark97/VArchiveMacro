1a931d5f96bf242baf091d0fd17aff9b1b9da34d
johypark97 <johypark97@gmail.com>
2025-01-10 21:46:18 +0900

    Replace Launch4j to Jlink

--- macro/build.gradle.kts
+++ macro/build.gradle.kts
@@ -1,9 +1,9 @@
-import edu.sc.seis.launch4j.tasks.DefaultLaunch4jTask
-import edu.sc.seis.launch4j.tasks.Launch4jLibraryTask
 import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
 
 val appName = "VArchive Macro"
+
+var isRelease = false
 
 version = Version.makeVersionString()
 
@@ -11,7 +11,7 @@ plugins {
     id("buildlogic.java-application-conventions")
     id("buildlogic.java-javacpp-conventions")
 
-    alias(libs.plugins.launch4j.plugin)
+    alias(libs.plugins.jlink.plugin)
 }
 
 dependencies {
@@ -27,8 +27,41 @@ application {
     mainModule = "varchivemacro.macro"
 }
 
+jlink {
+    options = listOf("--no-header-files", "--no-man-pages")
+
+    // slf4j - log4j2
+    addExtraDependencies("log4j-api")
+    addExtraDependencies("log4j-core")
+    addExtraDependencies("log4j-slf4j2-impl")
+    addExtraDependencies("slf4j-api")
+
+    mergedModule {
+        additive = true
+
+        // Fixes javax.net.ssl.SSLHandshakeException: Received fatal alert: handshake_failure.
+        requires("jdk.crypto.ec")
+
+        // slf4j - log4j2
+        run {
+            requires("java.naming") // Fixes the JNDI lookup class warning.
+
+            requires("org.apache.logging.log4j.core")
+            requires("org.apache.logging.log4j.slf4j2.impl")
+        }
+    }
+
+    launcher {
+        name = "macro v${version}"
+    }
+
+    jpackage {
+        icon = "jlink/icon.ico"
+    }
+}
+
 tasks.jar.get().doLast {
-    application.applicationDefaultJvmArgs += "-Dlog.level=ALL"
+    if (!isRelease) application.applicationDefaultJvmArgs += "-Dlog.level=ALL"
 }
 
 tasks.processResources {
@@ -43,69 +76,34 @@ tasks.register<WriteProperties>("buildProperties") {
     property("build.version", version)
 }
 
-tasks.withType<DefaultLaunch4jTask> {
-    outfile = "$appName v${project.version}.exe"
+tasks.jpackageImage.get().doLast {
+    val jPackageData = jlink.jpackageData.get()
+    val jPackageImageDir = "${jPackageData.imageOutputDir}/${jPackageData.imageName}"
 
-    headerType = "gui"
-    icon = "$projectDir/src/main/resources/icon.ico"
-    jvmOptions = application.applicationDefaultJvmArgs.toSet()
-    mainClassName = application.mainClass.get()
-
-    copyright = "johypark97"
-    fileDescription = rootProject.name
-    language = "KOREAN"
-    productName = rootProject.name
-    textVersion = project.version.toString()
-    version = project.version.toString()
-
-    doLast {
-        val path = "${layout.buildDirectory.get()}/${launch4j.outputDir.get()}"
-
-        copy {
-            from("${rootProject.projectDir}/LICENSE")
-            from("${rootProject.projectDir}/README.md")
-            into(path)
-        }
-
-        copy {
-            from("data")
-            into("$path/data")
-        }
-
-        copy {
-            from("${rootProject.projectDir}/doc")
-            into("$path/doc")
-        }
+    copy {
+        from("${rootProject.projectDir}/LICENSE")
+        from("${rootProject.projectDir}/README.md")
+        into(jPackageImageDir)
     }
-}
 
-tasks.register<Launch4jLibraryTask>("createExe_envJre") {
-    description = "Creates an executable using JAVA_HOME env JRE."
-    group = "launch4j"
-
-    outfile = "$appName env v${project.version}.exe"
-
-    bundledJrePath = "%JAVA_HOME%"
-}
-
-tasks.register<Launch4jLibraryTask>("createExe_localJre") {
-    description = "Creates an executable using local JRE."
-    group = "launch4j"
-
-    outfile = "$appName local v${project.version}.exe"
-
-    bundledJrePath = "jre17fx"
+    copy {
+        from("data")
+        into("$jPackageImageDir/data")
+    }
 }
 
 tasks.register<Zip>("release") {
     description = "Creates an archive file to release."
     group = "distribution"
 
-    dependsOn(tasks.createAllExecutables)
+    dependsOn(tasks.jpackageImage)
+
+    isRelease = true
 
     archiveBaseName = appName
     archiveVersion = version.toString()
 
-    from("${layout.buildDirectory.get()}/${launch4j.outputDir.get()}")
+    val jPackageData = jlink.jpackageData.get()
+    from("${jPackageData.imageOutputDir}/${jPackageData.imageName}")
     into("$appName v$version")
 }
