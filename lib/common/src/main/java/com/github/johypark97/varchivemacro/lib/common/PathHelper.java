34093c3f51b6bf103b773309c3aca96e6011e4bf
johypark97 <johypark97@gmail.com>
2025-02-02 14:05:13 +0900

    Refactoring PathHelper

--- lib/common/src/main/java/com/github/johypark97/varchivemacro/lib/common/PathHelper.java
+++ lib/common/src/main/java/com/github/johypark97/varchivemacro/lib/common/PathHelper.java
@@ -2,26 +2,24 @@ package com.github.johypark97.varchivemacro.lib.common;
 
 import java.nio.file.Path;
 
-public class PathHelper {
-    private final Path path;
-
-    public PathHelper(Path path) {
-        this.path = path;
+public record PathHelper(Path path) {
+    public boolean isSubPathOf(Path other) {
+        return isSubPathOf(other, false);
     }
 
-    public boolean isSubPathOf(Path other) {
-        return path.startsWith(other) && !path.equals(other);
+    public boolean isSubPathOf(Path other, boolean includeSelf) {
+        if (!path.startsWith(other)) {
+            return false;
+        }
+
+        return includeSelf || !path.equals(other);
     }
 
     public Path toRelativeOfOrNot(Path other) {
-        if (path.equals(other)) {
-            return Path.of(".");
+        if (!path.startsWith(other)) {
+            return path;
         }
 
-        if (path.startsWith(other)) {
-            return other.relativize(path);
-        }
-
-        return path;
+        return path.equals(other) ? Path.of(".") : other.relativize(path);
     }
 }
