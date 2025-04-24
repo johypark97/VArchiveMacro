0601b89c7b83f6cc40091b98328ec787fdcbbd8c
johypark97 <johypark97@gmail.com>
2025-04-24 22:10:10 +0900

    Remaking Macro - Delete domain layer classes

--- macro/src/main/java/com/github/johypark97/varchivemacro/macro/domain/scanner/model/RecordData.java
+++
@@ -1,9 +0,0 @@
-package com.github.johypark97.varchivemacro.macro.domain.scanner.model;
-
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleStringProperty;
-
-public class RecordData {
-    public final SimpleBooleanProperty maxCombo = new SimpleBooleanProperty();
-    public final SimpleStringProperty rateText = new SimpleStringProperty();
-}
