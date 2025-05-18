f840e222adb9bde27c528755bd25edb98c6aa4da
johypark97 <johypark97@gmail.com>
2025-05-18 15:59:35 +0900

    Remaking Macro - Refactoring UiResource

--- macro/src/main/java/com/github/johypark97/varchivemacro/macro/ui/resource/UiResource.java
+++ macro/src/main/java/com/github/johypark97/varchivemacro/macro/ui/resource/UiResource.java
@@ -3,30 +3,22 @@ package com.github.johypark97.varchivemacro.macro.ui.resource;
 import java.net.URL;
 import java.util.Objects;
 
-public class UiResource {
-    private static final String ICON_PATH = "/images/overMeElFail.png";
+public enum UiResource {
+    // icon
+    ICON("/images/overMeElFail.png"),
 
-    private static final String GLOBAL_CSS_FILE_NAME = "/fxml/styles/global.css";
-    private static final String TABLE_COLOR_CSS_FILE_NAME = "/fxml/styles/table-color.css";
-    private static final String TAB_COLOR_CSS_FILE_NAME = "/fxml/styles/tab-color.css";
+    // css
+    GLOBAL_CSS("/fxml/styles/global.css"),
+    TABLE_COLOR_CSS("/fxml/styles/table-color.css"),
+    TAB_COLOR_CSS("/fxml/styles/tab-color.css");
 
-    public static URL getIcon() {
-        URL url = UiResource.class.getResource(ICON_PATH);
-        return Objects.requireNonNull(url);
+    private final String path;
+
+    UiResource(String path) {
+        this.path = path;
     }
 
-    public static URL getGlobalCss() {
-        URL url = UiResource.class.getResource(GLOBAL_CSS_FILE_NAME);
-        return Objects.requireNonNull(url);
-    }
-
-    public static URL getTabColorCss() {
-        URL url = UiResource.class.getResource(TAB_COLOR_CSS_FILE_NAME);
-        return Objects.requireNonNull(url);
-    }
-
-    public static URL getTableColorCss() {
-        URL url = UiResource.class.getResource(TABLE_COLOR_CSS_FILE_NAME);
-        return Objects.requireNonNull(url);
+    public URL url() {
+        return Objects.requireNonNull(UiResource.class.getResource(path));
     }
 }
